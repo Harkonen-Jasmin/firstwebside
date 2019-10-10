0f5656e5f3b50f0bd6fb2690163b6cac5503e992
Jasmin Harkonen <jasminharkonen@Jasmins-MacBook-Pro.local>
2019-10-10 10:53:56 +0300

    My first page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <marquee>Tänä aamuna söin aamupalaa ja sanoin mummille heihei ja lähdin töihin</marquee>
+    </div>
+  );
+}
+
+export default App;
